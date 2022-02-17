89a9a106748fd642a22a77d7c8e0836a9d2a56fa
devjosecarlosteles <devjosecarlosteles@gmail.com>
2022-02-17 17:04:35 -0300

    initial configs to sequelize

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: '127.0.0.1',
+  username: "jctdcj",
+  password: "iron1704",
+  database: "apitdd",
+  dialect: "postgres",
+  operatorsAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
